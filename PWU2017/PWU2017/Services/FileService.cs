f94ec7d52b92cc44f802efbc786a633b092bf086
Artur Perec <artur.g.perec@gmail.com>
2017-01-25 05:35:30 +0100

    Project works again, MVVM in progress

---
+++ PWU2017/PWU2017/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWU2017.Services
+{
+    class FileService
+    {
+        public async Task SaveAsync(Models.FileInfo model)
+        {
+            if (model != null)
+                await Windows.Storage.FileIO.WriteTextAsync(model.Ref, model.Text);
+        }
+
+        public async Task<Models.FileInfo> LoadAsync(Windows.Storage.StorageFile file)
+        {
+            return new Models.FileInfo
+            {
+                Text = await Windows.Storage.FileIO.ReadTextAsync(file),
+                Name = file.DisplayName,
+                Ref = file,
+            };
+        }
+    }
+}
