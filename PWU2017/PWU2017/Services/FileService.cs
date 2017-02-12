aa59d1a84259caf6094fd09a1d01495558a764f1
Artur Perec <artur.g.perec@gmail.com>
2017-02-12 05:43:06 +0100

    Added JumpList

--- PWU2017/PWU2017/Services/FileService.cs
+++ PWU2017/PWU2017/Services/FileService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage.AccessCache;
 
 namespace PWU2017.Services
 {
@@ -16,6 +17,31 @@ namespace PWU2017.Services
 
         public async Task<Models.FileInfo> LoadAsync(Windows.Storage.StorageFile file)
         {
+            // add to jumplist
+            if (Windows.UI.StartScreen.JumpList.IsSupported())
+            {
+                var jlist = await Windows.UI.StartScreen.JumpList.LoadCurrentAsync();
+                jlist.SystemGroupKind = Windows.UI.StartScreen.JumpListSystemGroupKind.None;
+                while (jlist.Items.Count() > 4)
+                    jlist.Items.RemoveAt(jlist.Items.Count() - 1);
+                if (!jlist.Items.Any(x => x.Arguments == file.Path))
+                {
+                    var jitem = Windows.UI.StartScreen.JumpListItem.CreateWithArguments(file.Path, file.DisplayName);
+                    jlist.Items.Add(jitem);
+                }
+                await jlist.SaveAsync();
+            }
+
+            // add to mru
+            var mruList = StorageApplicationPermissions.MostRecentlyUsedList;
+            while (mruList.Entries.Count() >= mruList.MaximumItemsAllowed)
+                mruList.Remove(mruList.Entries.First().Token);
+            if (!mruList.Entries.Any(x => x.Metadata == file.Path))
+                mruList.Add(file, file.Path);
+            var FutureAccessList = StorageApplicationPermissions.FutureAccessList;
+            FutureAccessList.Add(file);
+
+            //build model
             return new Models.FileInfo
             {
                 Text = await Windows.Storage.FileIO.ReadTextAsync(file),
